358307c5347c93dc339009eeabede266f571f8d2
Fazlul Hoque <sawravm299@gmail.com>
2020-08-04 10:41:55 +0600

    language activity design complete

--- app/src/main/java/com/example/daktarlagbe/LanguageActivity.java
+++ app/src/main/java/com/example/daktarlagbe/LanguageActivity.java
@@ -2,14 +2,21 @@ package com.example.daktarlagbe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class LanguageActivity extends AppCompatActivity {
 
     ImageView langiageanim;
     AnimationDrawable frameAnimation;
+
+    Button engBtn, banglgaBtn;
+    ImageView previousBtn, nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,45 @@ public class LanguageActivity extends AppCompatActivity {
         langiageanim.setImageResource(R.drawable.languageanim);
         frameAnimation = (AnimationDrawable)langiageanim.getDrawable();
         frameAnimation.start();
+
+        previousBtn = findViewById(R.id.previousBtn);
+        nextBtn = findViewById(R.id.nextBtn);
+
+        engBtn = findViewById(R.id.engBtn);
+        banglgaBtn = findViewById(R.id.banglaBtn);
+
+        engBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                engBtn.setBackgroundColor(Color.parseColor("#00E2FC"));
+                engBtn.setTextColor(Color.parseColor("#ffffff"));
+            }
+        });
+
+        banglgaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                banglgaBtn.setBackgroundColor(Color.parseColor("#00E2FC"));
+                banglgaBtn.setTextColor(Color.parseColor("#ffffff"));
+            }
+        });
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                previousBtn.setBackgroundResource(R.drawable.color_arrow);
+                Intent preview = new Intent(LanguageActivity.this,WelcomeActivity.class);
+                startActivity(preview);
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextBtn.setBackgroundResource(R.drawable.color_arrow);
+                Intent next = new Intent(LanguageActivity.this,PhoneActivity.class);
+                startActivity(next);
+            }
+        });
     }
 }
