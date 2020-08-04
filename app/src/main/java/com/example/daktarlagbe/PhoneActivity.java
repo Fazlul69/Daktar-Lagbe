a69cb05e66dcfe97ed3d23cc930d1d0e5b314d7f
Fazlul Hoque <sawravm299@gmail.com>
2020-08-04 13:25:52 +0600

    phone number activity design
    almost complete

--- app/src/main/java/com/example/daktarlagbe/PhoneActivity.java
+++ app/src/main/java/com/example/daktarlagbe/PhoneActivity.java
@@ -1,14 +1,68 @@
 package com.example.daktarlagbe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class PhoneActivity extends AppCompatActivity {
+
+    LinearLayout phoneLogoLayout;
+    EditText phoneNumber;
+    ImageView phoneLogo;
+    ImageView previousBtn, nextBtn;
+    int logoChangeImage = R.drawable.logonumber;
+    int width = 230;
+    int height = 230;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
+
+        phoneLogoLayout = findViewById(R.id.phoneLogoLayout);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        phoneLogo = findViewById(R.id.phoneLogo);
+
+        previousBtn = findViewById(R.id.previousBtn);
+        nextBtn = findViewById(R.id.nextBtn);
+
+        final LinearLayout.LayoutParams ip =new LinearLayout.LayoutParams(width,height);
+
+        phoneNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                phoneLogoLayout.setBackgroundResource(R.drawable.phone_anim);
+                phoneNumber.setBackgroundResource(R.drawable.number_background_white);
+                phoneLogo.setImageResource(logoChangeImage);
+                phoneLogo.setLayoutParams(ip);
+            }
+        });
+
+        previousBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                previousBtn.setBackgroundResource(R.drawable.color_arrow);
+                Intent preview = new Intent(PhoneActivity.this,LanguageActivity.class);
+                startActivity(preview);
+            }
+        });
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextBtn.setBackgroundResource(R.drawable.color_arrow);
+                Intent next = new Intent(PhoneActivity.this,OtpActivity.class);
+                startActivity(next);
+            }
+        });
     }
 }
