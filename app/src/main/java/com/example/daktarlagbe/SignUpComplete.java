30f2081abb177a53dc2b9b93401193f582c21928
Fazlul Hoque <sawravm299@gmail.com>
2020-08-11 12:53:40 +0600

    signupcomplete activity
    complete with animation and splash screen

--- app/src/main/java/com/example/daktarlagbe/SignUpComplete.java
+++ app/src/main/java/com/example/daktarlagbe/SignUpComplete.java
@@ -2,13 +2,53 @@ package com.example.daktarlagbe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SignUpComplete extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN =2000;
+    Animation left_anim, right_anim, text_one_anim,text_two_anim;
+    ImageView leftAnimImage,rightAnimImage;
+    TextView textOneAnim;
+    LinearLayout textTwoAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up_complete);
+
+        left_anim = AnimationUtils.loadAnimation(this,R.anim.left_anim);
+        right_anim = AnimationUtils.loadAnimation(this,R.anim.right_anim);
+        text_one_anim = AnimationUtils.loadAnimation(this,R.anim.text_one_anim);
+        text_two_anim = AnimationUtils.loadAnimation(this,R.anim.text_two_anim);
+
+        leftAnimImage = findViewById(R.id.leftAnimImage);
+        rightAnimImage = findViewById(R.id.rightAnimImage);
+
+        textOneAnim = findViewById(R.id.textOneAnim);
+        textTwoAnim = findViewById(R.id.textTwoAnim);
+
+        textOneAnim.setAnimation(text_one_anim);
+        leftAnimImage.setAnimation(left_anim);
+        rightAnimImage.setAnimation(right_anim);
+        textTwoAnim.setAnimation(text_two_anim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SignUpComplete.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
